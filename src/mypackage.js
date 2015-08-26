4d5cf4c244b35234f410bec47b673a477b64b4d1
Yi-Cyuan <yicyuan.chen@lileesystems.com>
2015-08-26 15:50:37 +0800

    update

--- src/mypackage.js
+++ src/mypackage.js
@@ -1,7 +1,7 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['jquery'], factory);
+        define(['exports', 'jquery'], factory);
     } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
         // CommonJS
         factory(exports, require('jquery'));
